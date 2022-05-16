e8ffab8a3eeb13968d148588acb9c919ba2be323
The1024thmember <1786728683@qq.com>
2022-05-16 17:42:26 +1000

    adding IceCreamApp

---
+++ WebApplication2/Controllers/HelloWorldContoller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApplication2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            ViewData["name"] = name;
+            ViewData["id"] = ID;
+            return View();
+        }
+    }
+}
